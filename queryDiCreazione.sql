ab7d1642c3976d282bd26ecbbf0fd09b78db773b
CrisafulliPasquale <101709329+CrisafulliPasquale@users.noreply.github.com>
2023-11-30 11:57:08 +0100

    Add files via upload

---
+++ queryDiCreazione.sql
@@ -0,0 +1,37 @@
+CREATE TABLE Costo(
+  ID INTEGER PRIMARY KEY,
+  metodo_pagamento VARCHAR(30),
+  entita_prezzo CURRENCY
+);
+
+CREATE TABLE Esercizio (
+  ID INTEGER PRIMARY KEY,
+  serie INTEGER,
+  ripetizioni INTEGER,
+  recupero DATE
+);
+
+CREATE TABLE Cliente (
+    nome VARCHAR(30),
+    cognome VARCHAR(30),
+    paese VARCHAR(50),
+    password VARCHAR(255),
+    e_mail VARCHAR(100),
+    PRIMARY KEY (e_mail, password)
+);
+
+CREATE TABLE Gestore (
+    nome VARCHAR(255),
+    cognome VARCHAR(255),
+    paese VARCHAR(255),
+    e_mail VARCHAR(255),
+    codice_fiscale VARCHAR(255),
+    partita_iva VARCHAR(255),
+    PRIMARY KEY (codice_fiscale, partita_iva, e_mail)
+);
+
+CREATE TABLE Abbonamento (
+    ID INTEGER PRIMARY KEY,
+    fine DATE,
+    inizio DATE
+);
