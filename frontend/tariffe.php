fa68bf7e3e80d6f2e1cce44376b6aadc11a57673
CrisafulliPasquale <101709329+CrisafulliPasquale@users.noreply.github.com>
2024-01-08 12:30:39 +0000

    LAST

--- frontend/tariffe.php
+++ frontend/tariffe.php
@@ -56,6 +56,37 @@
         span {
             color: white;
         }
+
+        #platino {
+            background-color: #E5e4e2;
+            color: black;
+        }
+
+        #oro {
+            background-color: #d6a315;
+            color: white;
+        }
+
+        #argento {
+            background-color: #c0c0c0;
+            color: white;
+        }
+
+        button {
+            background-color: #e6720e;
+            color: white;
+            font-size: 18px;
+            padding: 10px 20px;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+            text-align: center;
+            transition: background-color 0.3s ease;
+        }
+
+        button:hover {
+            background-color: #ff9a44;
+        }
     </style>
 </head>
 <body>
@@ -67,8 +98,26 @@
         <a href="../frontend/login.php">Esci</a>
     </nav>
 
-    <div class="container">
+    <div class="container" id="argento">
         <h2>Abbonamento Silver</h2>
+        <h3>Prezzo: 50€/mese</h3>
+        <h4>Cosa include?</br> Scheda di allenamento</h4>
+        <a href='../frontend/pagamentoAbbonamento.php'><button>ACQUISTA ORA</button></a>
+    </div>
+
+
+    <div class="container" id="oro">
+        <h2>Abbonamento Gold</h2>
+        <h3>Prezzo: 100€/mese</h3>
+        <h4>Cosa include?</br> Scheda di allenamento con video-spiegazioni</h4>
+        <a href='../frontend/pagamentoAbbonamento.php'><button>ACQUISTA ORA</button></a>
+    </div>
+
+    <div class="container" id="platino">
+        <h2>Abbonamento Platinum</h2>
+        <h3>Prezzo: 200€/mese</h3>
+        <h4>Cosa include?</br> Scheda di allenamento con video-spiegazione e assistenza 24h</h4>
+        <a href='../frontend/pagamentoAbbonamento.php'><button>ACQUISTA ORA</button></a>
     </div>
 </body>
 </html>
